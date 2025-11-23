99049f3d8defa9d0bd9815762e3b48187b2f05c3
wuxianlin <wuxianlinwxl@gmail.com>
2025-11-23 22:50:24 +0800

    fix selinux denial

--- customize.sh
+++ customize.sh
@@ -1,2 +1,7 @@
 set_perm $MODPATH/system/vendor/bin/mosey_server 0 1000 0755 u:object_r:mosey_server_exec:s0
+mkdir -p $MODPATH/system/vendor/etc/selinux
+cp /vendor/etc/selinux/vendor_service_contexts $MODPATH/system/vendor/etc/selinux/
+[ "$(grep "mosey_service" $MODPATH/system/vendor/etc/selinux/vendor_service_contexts)" ] || echo "com.google.pixel.moseyservice.IMoseyService/default u:object_r:mosey_service:s0" >> $MODPATH/system/vendor/etc/selinux/vendor_service_contexts
+set_perm $MODPATH/system/vendor/etc/selinux/vendor_service_contexts 0 0 0644 u:object_r:vendor_configs_file:s0
+set_perm $MODPATH/system/vendor/etc/init/mosey.rc 0 0 0644 u:object_r:vendor_configs_file:s0
 
